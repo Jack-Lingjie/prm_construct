47e1a38555a1e84ad006ec5c9aa8078994246c7f
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-10-29 13:40:09 +0000

    modify install yaml

--- run_test.py
+++ run_test.py
@@ -8,18 +8,18 @@ from methods.bfs import bsf_solve
 # from tot.models import gpt_usage
 from models.mymodel import Generator
 
-SAVE_PATH = '/mnt/lingjiejiang/textual_aesthetics/prm/data'
+# SAVE_PATH = '/mnt/lingjiejiang/textual_aesthetics/prm/data'
 def run(args):
     task = get_task(args.task)
     model = Generator(args.model_name, temperature=args.temperature, max_tokens=args.max_tokens)
     logs, cnt_avg, cnt_any = [], 0, 0
     if args.naive_run:
-        file = f'{SAVE_PATH}/data/{args.task}_tree_path/{args.model_name}_{args.temperature}_naive_{args.prompt_sample}_sample_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
+        file = f'{args.save_path}/data/{args.task}_tree_path/{args.model_name}_{args.temperature}_naive_{args.prompt_sample}_sample_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
     else:
-        file = f'{SAVE_PATH}/data/{args.task}_tree_path/{args.model_name}_{args.temperature}_{args.model_name}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
+        file = f'{args.save_path}/data/{args.task}_tree_path/{args.model_name}_{args.temperature}_{args.model_name}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
     
 
-    path_file = f'{SAVE_PATH}/data/{args.task}_results/{args.model_name}_{args.temperature}_{args.model_name}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
+    path_file = f'{args.save_path}/data/{args.task}_results/{args.model_name}_{args.temperature}_{args.model_name}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
     
     os.makedirs(os.path.dirname(file), exist_ok=True)
     os.makedirs(os.path.dirname(path_file), exist_ok=True)
@@ -72,6 +72,7 @@ def parse_args():
     args.add_argument('--n_generate_sample', type=int, default=3)  # only thing needed if naive_run
     args.add_argument('--n_evaluate_sample', type=int, default=1)
     args.add_argument('--n_select_sample', type=int, default=1)
+    args.add_argument('--save_path', type=str, default="/mnt/lingjiejiang/textual_aesthetics/prm/data")
 
     args = args.parse_args()
     return args
